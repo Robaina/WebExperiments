64e093e220005d19b689bb443af129630f272a30
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2019-02-26 13:24:12 +0100

    separate languages

---
+++ script.js
@@ -0,0 +1,50 @@
+function draw() {
+  try {
+    // compile the expression
+    const expression = document.getElementById('eq').value;
+    const node = math.parse(expression);
+    const expr = node.compile();
+
+    // parse input
+    const xmin = document.getElementById('minval').value;
+    const xmax = document.getElementById('maxval').value;
+
+    // evaluate the expression repeatedly for different values of x
+    const xValues = math.range(parseFloat(xmin), parseFloat(xmax), 0.01).toArray();
+    const yValues = xValues.map(function (x) {
+      return expr.eval({x: x})
+    });
+
+    // render the plot using plotly
+    const trace1 = {
+      x: xValues,
+      y: yValues,
+      mode: 'lines',
+      line: {
+       //color: 'rgb(000, 000, 100)',
+       width: 3
+      }
+    };
+    const data = [trace1];
+    const layout = {
+      title:'$$f(x) = ' + node.toTex() + '$$',
+      xaxis:{title: '$$x$$'},
+      yaxis:{title: '$$f(x)$$'}
+    };
+
+    var plot = document.getElementById('plot');
+    Plotly.newPlot(plot, data, layout);
+
+  }
+  catch (err) {
+    console.error(err);
+    alert(err);
+  }
+}
+
+document.getElementById('form').onsubmit = function (event) {
+  event.preventDefault();
+  draw();
+}
+
+draw();
